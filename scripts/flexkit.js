02e150e112e0c001f2b07d0e08da572660f3b73e
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-12-16 12:02:01 -0500

    🎉 Added FlexKit

---
+++ scripts/flexkit.js
@@ -0,0 +1,31 @@
+/* ===[ ✅ GENERAL ]==================== */
+/* Longform */
+function append(e,t){t.appendChild(e)}
+/* Shortform */
+function ap(e,t){t.appendChild(e)}
+
+/* ===[ 🎯 SELECTORS ]================== */
+/* Longform */
+function getId(i){return document.getElementById(i)};
+function getCss(i){return document.querySelector(i)};
+function getAllCss(i){return document.querySelectorAll(i)};
+/* Shortform */
+function gi(i){return document.getElementById(i)};
+function qs(i){return document.querySelector(i)};
+function qsa(i){return document.querySelectorAll(i)};
+
+/* ===[ 🏗️ COMPLEX ]=================== */
+function addCSS(c){
+  let e = document.createElement('link');
+  e.rel='stylesheet';
+  e.href = c;
+  document.head.appendChild(e);
+  return e;
+}
+
+
+
+
+/* ===[ 📥 IMPORTS ]==================== */
+addCSS('https://cdn.jsdelivr.net/gh/ItsFoxDev/fa-turbo/css/all.min.css');
+addCSS('scripts/flexkit.css');
